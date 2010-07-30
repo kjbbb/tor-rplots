3f06d4f73cb6944a650e7f06e7370e4b5cffedd5
Kevin Berry <xckjb88@gmail.com>
2010-07-30 00:22:56 -0400

    Added run script to generate all graphs at once.

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -e "png" ]; then
+  mkdir png
+fi
+
+echo "Generating uptime box plots..."
+R --slave < uptime-boxplots.R | grep "TRUE"
+
+echo "Generating bandwidth box plots..."
+R --slave < bandwidth-boxplots.R | grep "TRUE"
+
+echo "Generating uptime histograms..."
+R --slave < uptime-histograms.R | grep "TRUE"
+
+echo "Generating bandwidth pie charts..."
+R --slave < bandwidth-piecharts.R | grep "TRUE"
+
+echo "Generating bandwidth/uptime bar chart..."
+R --slave < bandwidth-uptime-barchart.R | grep "TRUE"
+
+echo "Generating bandwidth histograms..."
+R --slave < bandwidth-histograms.R | grep "TRUE"
