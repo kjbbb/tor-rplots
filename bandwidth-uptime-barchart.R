d565ea84ed8381da001647a6f074ce84e5f09736
Kevin Berry <xckjb88@gmail.com>
2010-07-29 20:40:23 -0400

    Added a bar graph comparing bandwidth and uptime.

---
+++ bandwidth-uptime-barchart.R
@@ -0,0 +1,36 @@
+library("RPostgreSQL")
+library("ggplot2")
+
+db = "tordir"
+dbuser = "ernie"
+dbpassword= ""
+
+plot_bandwidth_uptime <- function() {
+
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, user=dbuser, password=dbpassword, dbname=db)
+
+
+  q <- paste("select (round(d.uptime/864000.0)*10)::integer as uptime, ",
+    "    avg(d.bandwidthavg)::integer as bwavg ",
+    "from descriptor d ",
+    "join statusentry s on d.descriptor=s.descriptor ",
+    "where d.bandwidthavg is not null ",
+    "    and date(s.validafter) >= '2010-02-01' ",
+    "    and date(s.validafter) <= '2010-02-05' ",
+    "group by (round(d.uptime/864000.0)*10)::integer ")
+
+  rs <- dbSendQuery(con, q)
+  bandwidth <- fetch(rs,n=-1)
+
+  ggplot(bandwidth) +
+    geom_bar(aes(x=uptime,y=bwavg), stat="identity")
+
+  ggsave(filename="png/bandwidth-uptime-bargraph.png", width=8, height=5, dpi=72)
+
+  #Close database connection
+  dbDisconnect(con)
+  dbUnloadDriver(drv)
+}
+
+plot_bandwidth_uptime()
